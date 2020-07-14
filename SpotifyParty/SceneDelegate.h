ac858579667018e2a9edd850852374c6714555b8
Diego de Jesus Ramirez <diegojrr@fb.com>
2020-07-14 13:09:21 -0500

    Initial Commit

---
+++ SpotifyParty/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  SpotifyParty
+//
+//  Created by Diego de Jesus Ramirez on 14/07/20.
+//  Copyright © 2020 DiegoRamirez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
