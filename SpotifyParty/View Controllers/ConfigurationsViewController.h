d7746006b0ecd4f96fadf60075ceb964838a29b2
Diego de Jesus Ramirez <diegojrr@fb.com>
2020-08-03 16:15:06 -0500

    Created a Configurations view controller

---
+++ SpotifyParty/View Controllers/ConfigurationsViewController.h
@@ -0,0 +1,17 @@
+//
+//  ConfigurationsViewController.h
+//  SpotifyParty
+//
+//  Created by Diego de Jesus Ramirez on 03/08/20.
+//  Copyright © 2020 DiegoRamirez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ConfigurationsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SpotifyParty/View Controllers/ConfigurationsViewController.m
@@ -0,0 +1,23 @@
+//
+//  ConfigurationsViewController.m
+//  SpotifyParty
+//
+//  Created by Diego de Jesus Ramirez on 03/08/20.
+//  Copyright © 2020 DiegoRamirez. All rights reserved.
+//
+
+#import "ConfigurationsViewController.h"
+
+@interface ConfigurationsViewController ()
+
+@end
+
+@implementation ConfigurationsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
