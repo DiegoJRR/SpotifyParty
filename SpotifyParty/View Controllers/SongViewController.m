cd59001e03f5833b8a9822a3ab2f7d1c4f080ab4
Diego de Jesus Ramirez <diegojrr@fb.com>
2020-08-06 14:08:34 -0500

    Added a song view controller to see specific information for a song
    This uses the ColorArt external library to understand the important colors of the album image, and use them in the view

---
+++ SpotifyParty/View Controllers/SongViewController.m
@@ -0,0 +1,52 @@
+//
+//  SongViewController.m
+//  SpotifyParty
+//
+//  Created by Diego de Jesus Ramirez on 05/08/20.
+//  Copyright © 2020 DiegoRamirez. All rights reserved.
+//
+
+#import "SongViewController.h"
+#import "APIManager.h"
+#import "UIImageView+AFNetworking.h"
+#include <ColorArt/UIImage+ColorArt.h>
+
+@interface SongViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *albumImageView;
+@property (weak, nonatomic) IBOutlet UILabel *songTitleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *authorNameLabel;
+
+@property (nonatomic, strong) APIManager *apiManager;
+
+@end
+
+@implementation SongViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.albumImageView setImageWithURL:[NSURL URLWithString: self.song.imageURL]];
+    self.albumImageView.layer.cornerRadius = 10;
+    
+    // Use external library to analyze the colors of the album colors
+    SLColorArt *colorArt = [self.albumImageView.image colorArt];
+    self.view.backgroundColor = colorArt.backgroundColor;
+    self.songTitleLabel.text = self.song.name;
+    self.songTitleLabel.textColor = colorArt.primaryColor;
+    
+    self.authorNameLabel.text = self.song.authorName;
+    self.authorNameLabel.textColor = colorArt.secondaryColor;
+    
+    self.navigationController.navigationBar.subviews.firstObject.alpha = 0;
+}
+
+#pragma mark - Navigation
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    
+    // Return the navigation controller to a solid color 
+    self.navigationController.navigationBar.subviews.firstObject.alpha = 1;
+
+}
+
+@end
